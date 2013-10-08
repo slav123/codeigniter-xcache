de4560c10bd78b5f687da7888072059658340361
slav123 <slav123@gmail.com>
2013-10-08 14:15:03 +1100

    is_set helper function & cache_info & get_metdata

--- libraries/Cache/drivers/Cache_xcache.php
+++ libraries/Cache/drivers/Cache_xcache.php
@@ -1,40 +1,42 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * CodeIgniter
  *
  * An open source application development framework for PHP 5.1.6 or newer
  *
- * @package		CodeIgniter
- * @author		Slawomir Jasinski
- * @copyright	Copyright (c) 2010 - 2011 SpiderSoft
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 2.0
- * @filesource	
+ * @package        CodeIgniter
+ * @author         Slawomir Jasinski
+ * @copyright      Copyright (c) 2010 - 2013 SpiderSoft
+ * @license        http://codeigniter.com/user_guide/license.html
+ * @link           http://codeigniter.com
+ * @since          Version 2.0
+ * @filesource
  */
 
 // ------------------------------------------------------------------------
 
 /**
- * CodeIgniter XCache Caching Class 
+ * CodeIgniter XCache Caching Class
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Core
- * @author		Slawomir Jasinski
- * @link		
+ * @package        CodeIgniter
+ * @subpackage     Libraries
+ * @category       Core
+ * @author         Slawomir Jasinski
+ * @link
  */
 
-class CI_Cache_xcache extends CI_Driver {
+class CI_Cache_xcache extends CI_Driver
+{
 
 	/**
-	 * Get 
+	 * Get
 	 *
-	 * Look for a value in the cache.  If it exists, return the data 
+	 * Look for a value in the cache.  If it exists, return the data
 	 * if not, return FALSE
 	 *
-	 * @param 	string	
-	 * @return 	mixed		value that is stored/FALSE on failure
+	 * @param    string
+	 *
+	 * @return    mixed        value that is stored/FALSE on failure
 	 */
 	public function get($id)
 	{
@@ -44,28 +46,30 @@ class CI_Cache_xcache extends CI_Driver {
 	}
 
 	// ------------------------------------------------------------------------	
-	
+
 	/**
 	 * Cache Save
 	 *
-	 * @param 	string		Unique Key
-	 * @param 	mixed		Data to store
-	 * @param 	int			Length of time (in seconds) to cache the data
+	 * @param    string         Unique Key
+	 * @param    mixed          Data to store
+	 * @param    int            Length of time (in seconds) to cache the data
 	 *
-	 * @return 	boolean		true on success/false on failure
+	 * @return    boolean        true on success/false on failure
 	 */
 	public function save($id, $data, $ttl = 60)
 	{
 		return xcache_set($id, $data, $ttl);
 	}
-	
+
 	// ------------------------------------------------------------------------
 
 	/**
 	 * Delete from Cache
 	 *
-	 * @param 	mixed		unique identifier of the item in the cache
-	 * @param 	boolean		true on success/false on failure
+	 * @param    mixed          unique identifier of the item in the cache
+	 * @param    boolean        true on success/false on failure
+	 *
+	 * @return bool
 	 */
 	public function delete($id)
 	{
@@ -81,18 +85,58 @@ class CI_Cache_xcache extends CI_Driver {
 	 */
 	public function is_supported()
 	{
-		if ( ! extension_loaded('xcache') OR ! function_exists('xcache_get'))
-		{
+		if (! extension_loaded('xcache') OR ! function_exists('xcache_get')) {
 			log_message('error', 'The XCache PHP extension must be loaded to use XCache.');
+
 			return FALSE;
 		}
-		
+
 		return TRUE;
+	}
+
+	/**
+	 * is_set in cache
+	 *
+	 * @param mixed       unique identifier of the item in the cache
+	 * @param boolean     true on success/false on failure
+	 *
+	 * @return bool
+	 */
+
+	public function is_set($id)
+	{
+		return xcache_isset($id);
 	}
 
 	// ------------------------------------------------------------------------
 
-	
+	/**
+	 * Cache Info
+	 *
+	 * @param    string        user/filehits
+	 *
+	 * @return    boolean        FALSE
+	 */
+	public function cache_info($type = NULL)
+	{
+		return FALSE;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Get Cache Metadata
+	 *
+	 * @param    mixed        key to get cache metadata on
+	 *
+	 * @return    boolean        FALSE
+	 */
+	public function get_metadata($id = NULL)
+	{
+		return FALSE;
+	}
+
+
 }
 // End Class
 
